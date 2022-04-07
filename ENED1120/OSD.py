4c6afc76f1e100b4ec32f2bf754db0016167c9df
Xvraiden <xvraiden@gmail.com>
2022-04-07 19:15:24 -0400

    updated font

--- ENED1120/OSD.py
+++ ENED1120/OSD.py
@@ -1,7 +1,9 @@
 from ev3dev2.display import Display
+import ev3dev2.fonts as fonts
+
 
 #show the text as input
 def Draw(text):
     m_Display = Display()
 
-    m_Display.text_grid(text, True, 0, 6)
+    m_Display.text_grid(text, True, 0, 6, 'black', font=fonts.load('charB124'))
