a681a109429d07ca10875604fb77af97d5d0b88d
Xvraiden <xvraiden@gmail.com>
2022-04-07 19:11:55 -0400

    Removed sleep

--- ENED1120/Drivetrain.py
+++ ENED1120/Drivetrain.py
@@ -1,11 +1,5 @@
-from time import sleep
-import math
-
 
 def AvoidDrive(Speed, x, y, yFirst, m_Drivetrain, m_Ultrasonic):
-
-    xTarget = x
-    yTarget = y
 
     x = x - m_Drivetrain.x_pos_mm
     y = y - m_Drivetrain.y_pos_mm
@@ -15,7 +9,6 @@ def AvoidDrive(Speed, x, y, yFirst, m_Drivetrain, m_Ultrasonic):
             m_Drivetrain.turn_to_angle(Speed, 0)
         elif (x < 0):
             m_Drivetrain.turn_to_angle(Speed, 180)
-        sleep(2)
         x = abs(x)
         while (x > 0):
             if (m_Ultrasonic.distance_centimeters > 20):
@@ -25,30 +18,11 @@ def AvoidDrive(Speed, x, y, yFirst, m_Drivetrain, m_Ultrasonic):
                 else:
                     m_Drivetrain.on_for_distance(Speed,x)
                     x = 0
-        sleep(2)
-        if (y > 0):
-            m_Drivetrain.turn_to_angle(Speed, 90)
-        elif (y < 0):
-            m_Drivetrain.turn_to_angle(Speed, 270)
-        sleep(2)
-        y = abs(y)
-        while (y > 0):
-            if (m_Ultrasonic.distance_centimeters > 10):
-                if (y >= 10):
-                    m_Drivetrain.on_for_distance(Speed,10,False)
-                    y = y - 10
-                else:
-                    m_Drivetrain.on_for_distance(Speed,y)
-                    y = 0
-        sleep(2)
-        m_Drivetrain.on_to_coordinates(Speed, xTarget, yTarget)
-    else:
         if (y > 0):
             m_Drivetrain.turn_to_angle(Speed, 90)
         elif (y < 0):
             m_Drivetrain.turn_to_angle(Speed, 270)
         y = abs(y)
-        sleep(2)
         while (y > 0):
             if (m_Ultrasonic.distance_centimeters > 10):
                 if (y >= 10):
@@ -58,13 +32,26 @@ def AvoidDrive(Speed, x, y, yFirst, m_Drivetrain, m_Ultrasonic):
                     m_Drivetrain.on_for_distance(Speed,y)
                     y = 0
 
-        sleep(2)
+    else:
+        if (y > 0):
+            m_Drivetrain.turn_to_angle(Speed, 90)
+        elif (y < 0):
+            m_Drivetrain.turn_to_angle(Speed, 270)
+        y = abs(y)
+        while (y > 0):
+            if (m_Ultrasonic.distance_centimeters > 10):
+                if (y >= 10):
+                    m_Drivetrain.on_for_distance(Speed,10,False)
+                    y = y - 10
+                else:
+                    m_Drivetrain.on_for_distance(Speed,y)
+                    y = 0
+
         if (x > 0):
             m_Drivetrain.turn_to_angle(Speed, 0)
         elif (x < 0):
             m_Drivetrain.turn_to_angle(Speed, 180)
         x = abs(x)
-        sleep(2)
         while (x > 0):
             if (m_Ultrasonic.distance_centimeters > 10):
                 if (x >= 10):
@@ -73,5 +60,3 @@ def AvoidDrive(Speed, x, y, yFirst, m_Drivetrain, m_Ultrasonic):
                 else:
                     m_Drivetrain.on_for_distance(Speed,x)
                     x = 0
-        sleep(2)
-        m_Drivetrain.on_to_coordinates(Speed, xTarget, yTarget)
